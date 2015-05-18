d5a6f7008728bcbe0703b9db5d468227accc6ecb
Thais Martins <thaismartinsweb@gmail.com>
2015-05-17 23:51:34 -0300

    Estilo dos botões da página inicial

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,35 +12,42 @@ app.controller('MainCtrl', function ($scope, MenuService) {
 	  
 	$scope.menu = MenuService.getItems();
 	
+	$scope.networks = [
+	    { name:'Linkedin', icon:'linkedin-square', link:'#' },
+	    { name:'Github', icon:'github-alt', link:'#' },
+	];
+	
 	$scope.skills = [
-	                 { id:'xsmall', name:'Doctrine ORM' },
-	         		{ id:'small', name:'CodeIgniter' },
-	         		{ id:'xbig', name:'PHP' },
-	         		{ id:'xsmall', name:'Yii Framework' },
-	         		{ id:'small', name:'CakePHP' },
-	         		{ id:'medium', name:'Spring' },
-	         		{ id:'xbig', name:'Java' },
-	         		{ id:'big', name:'Hibernate' },
-	         		{ id:'medium', name:'Apache' },
-	         		{ id:'xsmall', name:'Ngnix' },
-	         		{ id:'medium', name:'PHP-FPM' },
-	         		{ id:'medium', name:'JBoss' },
-	         		{ id:'xsmall', name:'TomCat' },
-	         		{ id:'small', name:'Jenkins' },
-	         		{ id:'xsmall', name:'Restfull' },
-	         		{ id:'medium', name:'MySQL' },
-	         		{ id:'big', name:'Redis' },
-	         		{ id:'xsmall', name:'Linux' },
-	         		{ id:'big', name:'AWS' },
-	         		{ id:'small', name:'Eclipse' },
-	         		{ id:'medium', name:'Git' },
-	         		{ id:'xsmall', name:'SVN' },
-	         		{ id:'big', name:'Orientação a Objetos' },
-	         		{ id:'medium', name:'Padrões de Projetos' },
-	         		{ id:'small', name:'TDD' },
-	         		{ id:'big', name:'Engenharia de Requisitos' },
-	         		{ id:'xsmall', name:'Scrum' },
-	         		{ id:'medium', name:'Kanban' },
-	         		{ id:'small', name:'Lean' }
-	             ];
+	    { id:'xsmall', name:'Doctrine ORM' },
+		{ id:'small', name:'CodeIgniter' },
+		{ id:'xbig', name:'PHP' },
+		{ id:'xsmall', name:'Yii Framework' },
+		{ id:'small', name:'CakePHP' },
+		{ id:'medium', name:'Spring' },
+		{ id:'xbig', name:'Java' },
+		{ id:'big', name:'Hibernate' },
+		{ id:'medium', name:'Apache' },
+		{ id:'xsmall', name:'Ngnix' },
+		{ id:'medium', name:'PHP-FPM' },
+		{ id:'medium', name:'JBoss' },
+		{ id:'xsmall', name:'TomCat' },
+		{ id:'small', name:'Jenkins' },
+		{ id:'xsmall', name:'Restfull' },
+		{ id:'medium', name:'MySQL' },
+		{ id:'big', name:'Redis' },
+		{ id:'xsmall', name:'Linux' },
+		{ id:'big', name:'AWS' },
+		{ id:'small', name:'Eclipse' },
+		{ id:'medium', name:'Git' },
+		{ id:'xsmall', name:'SVN' },
+		{ id:'big', name:'Orientação a Objetos' },
+		{ id:'medium', name:'Padrões de Projetos' },
+		{ id:'small', name:'TDD' },
+		{ id:'big', name:'Engenharia de Requisitos' },
+		{ id:'xsmall', name:'Scrum' },
+		{ id:'medium', name:'Kanban' },
+		{ id:'small', name:'Lean' }
+   ];
+	
+	
 });
