8b4915fc9a52e1a93ecaf338426156edbe5eb22f
Thais Martins <thaismartinsweb@gmail.com>
2015-05-22 18:42:58 -0300

    Criação do visual da página de sistemas

--- app/scripts/controllers/systems.js
+++ app/scripts/controllers/systems.js
@@ -13,11 +13,17 @@ app.controller('SystemsCtrl', function ($scope, MenuService) {
 	$scope.menu = MenuService.getItems();
     
 	$scope.skills = [
-   	  {name:'HTML5', image:''},
-   	  {name:'CSS3', image:''},
-   	  {name:'Bootstrap', image:''},
-   	  {name:'Javascript', image:''},
-   	  {name:'AngularJs', image:''},
-   	  {name:'Cross-Browser', image:''},
+   	  {name:'PHP', icon:'code'},
+   	  {name:'Yii', icon:'cubes'},
+   	  {name:'Codeigniter', icon:'cubes'},
+   	  {name:'Doctrine', icon:'cogs'},
+   	  {name:'Ngnix/PHP-FPM', icon:'hdd-o'},
+   	  {name:'Java', icon:'code'},
+   	  {name:'Hibernate', icon:'cogs'},
+   	  {name:'Spring', icon:'cubes'},
+   	  {name:'Jboss', icon:'hdd-o'},
+   	  {name:'Tomcat', icon:'hdd-o'},
+   	  {name:'git', icon:'git'},
+ 	  {name:'MySql', icon:'database'},
    	];
 });
