b1ddbbc47e10b93347dd8a64a0edc6ff4747e096
Thais Martins <thaismartinsweb@gmail.com>
2015-05-23 16:03:32 -0300

    Criação dos detalhes de projetos

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -8,11 +8,23 @@
  * Controller of the thaisMartins
  */
 var app = angular.module('thaisMartins');
-app.controller('ProjectsCtrl', function ($scope, MenuService, ProjectsService) {
+app.controller('ProjectsCtrl', function ($scope, $filter, MenuService, ProjectsService) {
 	  
 	$scope.menu = MenuService.getItems();
-	
 	$scope.projects = ProjectsService.getItems();
 	
-	$scope.baseUrl = '/images/projects/';
+	$scope.baseUrl = 'images/projects/';
+	$scope.showModal = false;
+	
+    $scope.showDetails = function(item){
+    	
+    	if(item === null) {
+    		return;
+    	}
+    	
+    	$scope.showModal = true;
+        $scope.modalItem = item;
+        console.log($scope.modalItem);
+    };
+    
 });
